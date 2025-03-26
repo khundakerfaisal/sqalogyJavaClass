de00a592bca551b646e2dc6dcd0a0e7f5505318d
khundakerfaisal <khundakermis@gmail.com>
2025-03-26 17:58:44 +0600

    sqalogy forst class

---
+++ src/test/java/SqalogyClasses/First_class.java
@@ -0,0 +1,13 @@
+package SqalogyClasses;
+
+public class First_class {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps");
+        String name="Farisha";
+        int age=30;
+        String neighbour="namira";
+        String friend=neighbour;
+        System.out.println(name);
+
+    }
+}
---
+++ src/test/java/SqalogyClasses/SecondClassVeriableType.java
@@ -0,0 +1,23 @@
+package SqalogyClasses;
+
+public class SecondClassVeriableType {
+    //byte - 1 [-128 to 127 ]
+    //short - 2
+    //int - 4 [1,2,3]
+    //long -8,
+    //float-4 [3.14]
+    //double-8
+    //char -2 [a,b,c]
+    //boolean- 1 [True /False]
+
+    /* Added veriable type Example*/
+    /* Premative datatype */
+    byte age =30;
+    int phone=1234567890;
+    long phoneNumber =12345678900L; //If long in the end of the number added "L"
+    float pi=3.14F;//If Float in the end of the number added "F"
+    char letter='@';
+    boolean isAdult= true;
+    /* End veriable type Example*/
+
+}
