4b9db5bd89283196ff0edd335673dbc8728b1b46
khundakerfaisal <khundakermis@gmail.com>
2025-04-17 13:09:03 +0600

    added multiple number random findings

--- src/test/java/SqalogyClasses/numberFindClass.java
+++ src/test/java/SqalogyClasses/numberFindClass.java
@@ -1,4 +1,32 @@
 package SqalogyClasses;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class numberFindClass {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int myNumber=(int)(Math.random()*100);
+//        System.out.println(myNumber);
+        int userNumber=0;
+        do {
+            System.out.println("Guese the number : ");
+            userNumber=sc.nextInt();
+            if (userNumber == myNumber){
+                System.out.println("Woohoo .. Correct the number !!!");
+                break;
+            }
+            else if (userNumber>myNumber){
+                System.out.println("User number is too large !!!");
+            }
+            else {
+                System.out.println("your number is too small !!!");
+            }
+        }while (userNumber>=0);
+        System.out.println("My number was : ");
+        System.out.println(myNumber);
+
+
+    }
+
 }
