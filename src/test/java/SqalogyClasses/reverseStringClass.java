969738132b9692869106e0ff375e689c4c7ac006
khundakerfaisal <khundakermis@gmail.com>
2025-06-04 19:24:07 +0600

    added reverse name

--- src/test/java/SqalogyClasses/reverseStringClass.java
+++ src/test/java/SqalogyClasses/reverseStringClass.java
@@ -3,18 +3,30 @@ package SqalogyClasses;
 public class reverseStringClass {
     public static void main(String[] args) {
         //Using String builder function
-        String areaName="akahd";
+//        String areaName="akahd";
+//        StringBuilder sb=new StringBuilder(areaName);
+//        String reverseName=sb.reverse().toString();
+//        System.out.println("Reverse name is " +reverseName);
+//
+//        //Using stractural program to reverse the name
+//        char [] reverseCheck=areaName.toCharArray();
+//        String reverse="";
+//        for (int i=reverseCheck.length-1;i>=0;i--){
+//            reverse +=reverseCheck[i];
+//        }
+//        System.out.println("The name is " +reverse);
+
+        String areaName="Feni";
         StringBuilder sb=new StringBuilder(areaName);
-        String reverseName=sb.reverse().toString();
-        System.out.println("Reverse name is " +reverseName);
+        String reverName=sb.reverse().toString();
+        System.out.println(reverName);
+        char [] checkArea=areaName.toCharArray();
+        String reverseName="";
+        for (int i=checkArea.length-1;i>=0;i--){
+            reverseName+=checkArea[i];
 
-        //Using stractural program to reverse the name
-        char [] reverseCheck=areaName.toCharArray();
-        String reverse="";
-        for (int i=reverseCheck.length-1;i>=0;i--){
-            reverse +=reverseCheck[i];
+
         }
-        System.out.println("The name is " +reverse);
-
+        System.out.println(reverseName);
     }
 }
