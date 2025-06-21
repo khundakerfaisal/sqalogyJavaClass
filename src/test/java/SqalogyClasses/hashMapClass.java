f4a03105dfe52dfa30eb57b2fdacc005d140aca2
khundakerfaisal <khundakermis@gmail.com>
2025-06-21 16:51:17 +0600

    added practise clas 3

--- src/test/java/SqalogyClasses/hashMapClass.java
+++ src/test/java/SqalogyClasses/hashMapClass.java
@@ -11,13 +11,13 @@ public class hashMapClass {
         mapData.put("Mamun",50);
         mapData.put("Zahid",30);
         mapData.put("Mahbub",60);
-        System.out.println(mapData);                               //Get all json key value data
+        System.out.println("Print all data " +mapData);                               //Get all json key value data
         mapData.remove("Arman",40);                                //Remove the data
-        System.out.println(mapData);
+        System.out.println("Print only remove data" +mapData);
         mapData.replace("Mahbub",60,45);    //Replace the data
-        System.out.println(mapData);
+        System.out.println("Print only replace data" +mapData);
         Integer getData=mapData.get("Mamun");                     //Get only one index data
-        System.out.println(getData);
+        System.out.println("Get only one index from the data" +getData);
 
     }
 }
---
+++ src/test/java/SqalogyClasses/practiseClass3.java
@@ -0,0 +1,23 @@
+package SqalogyClasses;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class practiseClass3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String generateNumber= String.valueOf(Math.round(Math.random()*100));
+        System.out.println(generateNumber);
+        System.out.println("Input Number :");
+        String InputNumber=sc.nextLine();
+
+
+        if (InputNumber.equals(generateNumber)){
+            System.out.println("Yahoo!! You are input correct Number");
+        }else {
+            System.out.println("Your are input wrong number");
+        }
+
+    }
+
+}
