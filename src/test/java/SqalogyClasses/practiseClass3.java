6ae46a7ac12bd8e4874e270864700e1e34207458
khundakerfaisal <khundakermis@gmail.com>
2025-06-22 16:18:53 +0600

    added loop with print example

--- src/test/java/SqalogyClasses/practiseClass3.java
+++ src/test/java/SqalogyClasses/practiseClass3.java
@@ -5,21 +5,40 @@ import java.util.Scanner;
 
 public class practiseClass3 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String generateNumber= String.valueOf(Math.round(Math.random()*100));
-        System.out.println(generateNumber);
+//        Scanner sc=new Scanner(System.in);
+//        String generateNumber= String.valueOf(Math.round(Math.random()*100));
+//        System.out.println(generateNumber);
+//
+//        String inputNumber = "";
+//
+//        while (!inputNumber.equals(generateNumber)){
+//            System.out.println("Input Number :");
+//            String InputNumber=sc.nextLine();
+//            if (InputNumber.equals(generateNumber)){
+//                System.out.println("Yahoo!! You are input correct Number");
+//            }else {
+//                System.out.println("Your are input wrong number");
+//            }
+//        }
 
-        String inputNumber = "";
+        int inatializeNumber=5;
 
-        while (!inputNumber.equals(generateNumber)){
-            System.out.println("Input Number :");
-            String InputNumber=sc.nextLine();
-            if (InputNumber.equals(generateNumber)){
-                System.out.println("Yahoo!! You are input correct Number");
-            }else {
-                System.out.println("Your are input wrong number");
+            for(int i=0;i<=inatializeNumber;i++){
+                System.out.println("Print Number is :" +i);
             }
-        }
+
+
+//            while (inatializeNumber<10){
+//                System.out.println("while print value is " +inatializeNumber);
+//                inatializeNumber++;
+//            }
+
+            do{
+                System.out.println("do-while print value is " +inatializeNumber);
+                inatializeNumber++;
+            }while (inatializeNumber<=8);
+
+
 
 
     }
