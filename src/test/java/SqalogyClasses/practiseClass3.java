b899ff710ef83a23698e1efbbb2b78f298eb45db
khundakerfaisal <khundakermis@gmail.com>
2025-06-23 13:13:02 +0600

    arraylist hashset and assending program added

--- src/test/java/SqalogyClasses/practiseClass3.java
+++ src/test/java/SqalogyClasses/practiseClass3.java
@@ -1,7 +1,6 @@
 package SqalogyClasses;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class practiseClass3 {
     public static void main(String[] args) {
@@ -21,22 +20,72 @@ public class practiseClass3 {
 //            }
 //        }
 
-        int inatializeNumber=5;
-
-            for(int i=0;i<=inatializeNumber;i++){
-                System.out.println("Print Number is :" +i);
-            }
-
-
-//            while (inatializeNumber<10){
-//                System.out.println("while print value is " +inatializeNumber);
-//                inatializeNumber++;
+//        int inatializeNumber=5;
+//
+//            for(int i=0;i<=inatializeNumber;i++){
+//                System.out.println("Print Number is :" +i);
 //            }
+//
+//
+////            while (inatializeNumber<10){
+////                System.out.println("while print value is " +inatializeNumber);
+////                inatializeNumber++;
+////            }
+//
+//            do{
+//                System.out.println("do-while print value is " +inatializeNumber);
+//                inatializeNumber++;
+//            }while (inatializeNumber<=8);
 
-            do{
-                System.out.println("do-while print value is " +inatializeNumber);
-                inatializeNumber++;
-            }while (inatializeNumber<=8);
+//       for (int i=0;i<=5;i++){
+//           if (i==4){
+//               continue;
+//           }
+//           System.out.println("Print the continue program value "+i);
+//       }
+//
+//        for (int i=0;i<=5;i++){
+//            if (i==5){
+//                break;
+//            }
+//            System.out.println("Print the break program value " +i);
+//        }
+
+        HashSet<Integer> set=new HashSet<>(); //Hashmap program
+        set.add(20);
+        set.add(10);
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        System.out.println(set);
+        ArrayList<Integer> listData=new ArrayList<>(set);
+        Collections.sort(listData);
+        System.out.println(listData.get(1));
+        System.out.println(listData.remove(0));
+        System.out.println(listData);
+
+        String  name="Farisha"; //Reverse string program
+        char [] reverse=name.toCharArray();
+        String reverseName="";
+        for (int i=reverse.length-1;i>=0;i--)
+        {
+            reverseName+=reverse[i];
+        }
+        System.out.println(reverseName);
+
+        int[] number={30,20,40}; //Array Sort
+        for (int i=0;i<number.length;i++){
+            for (int j=i+1;j<number.length;j++){
+                if (number[i]>number[j]){
+                    int temp=number[i];
+                    number[i]=number[j];
+                    number[j]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(number));
+
+
 
 
 
