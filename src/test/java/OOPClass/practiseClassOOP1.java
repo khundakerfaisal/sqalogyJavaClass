ce3c87dc50ee7d051768ae5cf531cd77de488622
khundakerfaisal <khundakermis@gmail.com>
2025-07-07 16:13:29 +0600

    added data his=den property

--- src/test/java/OOPClass/practiseClassOOP1.java
+++ src/test/java/OOPClass/practiseClassOOP1.java
@@ -2,40 +2,80 @@ package OOPClass;
 
 public class practiseClassOOP1 {
 
-
-
     private String empName;
-    private int grossSalary;
+    private String department;
     private int hour;
-    private int perDayAmount;
-    private int month;
-
-
-    public String getEmpName(){
-        return empName;
-    }
-    public void setEmpName(String empName){
-        this.empName=empName;
-    }
+    private int amount;
+    private int overtime;
+    private int grossSalary;
 
     public int getGrossSalary(){
         return grossSalary;
     }
-    public void setGrossSalary(int hour,int perDayAmount,int month){
+
+    public void setGrossSalary(int hour,int amount,int overtime){
         this.hour=hour;
-        this.perDayAmount=perDayAmount;
-        this.grossSalary=((hour*perDayAmount)*month);
+        this.amount=amount;
+        this.overtime=overtime;
+        this.grossSalary=((hour*amount)+overtime);
+
     }
 
+    public String getEmpName(){
+        return empName;
+    }
+
+
+
+    public void setEmpName(String empName){
+        this.empName=empName;
+
+    }
 
     public static void main(String[] args) {
-        practiseClassOOP1 poop1=new practiseClassOOP1();
-        poop1.setEmpName("Arman");
-        poop1.setGrossSalary(8,1500,26);
-        System.out.println("Emp name is : "+poop1.getEmpName());
-        System.out.println("Emp salary is : "+poop1.getGrossSalary());
+        practiseClassOOP1 pop=new practiseClassOOP1();
+        pop.setEmpName("Mamun");
+        System.out.println(pop.getEmpName());
 
+        pop.setGrossSalary(8,1500,20);
+        System.out.println(pop.getGrossSalary());
     }
+
+
+
+
+//    private String empName;
+//    private int grossSalary;
+//    private int hour;
+//    private int perDayAmount;
+//    private int month;
+//
+//
+//    public String getEmpName(){
+//        return empName;
+//    }
+//    public void setEmpName(String empName){
+//        this.empName=empName;
+//    }
+//
+//    public int getGrossSalary(){
+//        return grossSalary;
+//    }
+//    public void setGrossSalary(int hour,int perDayAmount,int month){
+//        this.hour=hour;
+//        this.perDayAmount=perDayAmount;
+//        this.grossSalary=((hour*perDayAmount)*month);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        practiseClassOOP1 poop1=new practiseClassOOP1();
+//        poop1.setEmpName("Arman");
+//        poop1.setGrossSalary(8,1500,26);
+//        System.out.println("Emp name is : "+poop1.getEmpName());
+//        System.out.println("Emp salary is : "+poop1.getGrossSalary());
+//
+//    }
 
 
 }
