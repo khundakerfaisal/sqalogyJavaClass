4a150f1f6b1de5ae2ccaa3d406be673ebabab302
khundakerfaisal <khundakermis@gmail.com>
2025-08-06 17:00:08 +0600

    added static variable

--- src/test/java/problemSolvingJava/NtechClass1.java
+++ src/test/java/problemSolvingJava/NtechClass1.java
@@ -1,12 +1,28 @@
 package problemSolvingJava;
 
 public class NtechClass1 {
+    //Instance variable class
 
-    int age;
+        //    int age; //instance variable
+        //    public static void main(String[] args) {
+        //        NtechClass1 ntechClass1=new NtechClass1();
+        //       ntechClass1.age=10;
+        //        System.out.println("Age is :" + ntechClass1.age);
+        //    }
+    //Static variable
+    static int age = 20;
     public static void main(String[] args) {
-        NtechClass1 ntechClass1=new NtechClass1();
-       ntechClass1.age=10;
-        System.out.println("Age is :" + ntechClass1.age);
+        System.out.println("Nahid Age is " + age);
     }
+    //Local Variable
+//    String name = "Stein";//Local Variable
+//
+//        public static void main(String[] args) {
+//        NtechClass1 ntechClass1=new NtechClass1();
+//        System.out.println("Nahid age is :" + ntechClass1.name);
+//    }
+//    static {        //first execute static method
+//        System.out.println("I have learn lot of thing!!!");
+//    }
 
 }
