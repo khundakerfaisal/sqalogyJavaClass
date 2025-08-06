997f4333d7409d6c84dc64e5406bea2a8bf16c60
khundakerfaisal <khundakermis@gmail.com>
2025-08-06 16:51:42 +0600

    added instance variable example

---
+++ src/test/java/problemSolvingJava/NtechClass1.java
@@ -0,0 +1,12 @@
+package problemSolvingJava;
+
+public class NtechClass1 {
+
+    int age;
+    public static void main(String[] args) {
+        NtechClass1 ntechClass1=new NtechClass1();
+       ntechClass1.age=10;
+        System.out.println("Age is :" + ntechClass1.age);
+    }
+
+}
