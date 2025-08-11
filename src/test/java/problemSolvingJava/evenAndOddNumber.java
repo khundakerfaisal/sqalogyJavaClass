cce808e9d509398419ffc63fcb8d3282081fb652
khundakerfaisal <khundakermis@gmail.com>
2025-08-11 16:55:47 +0600

    added input number from system to find even odd

--- src/test/java/problemSolvingJava/evenAndOddNumber.java
+++ src/test/java/problemSolvingJava/evenAndOddNumber.java
@@ -1,29 +1,51 @@
 package problemSolvingJava;
 
+import java.util.Scanner;
+
 public class evenAndOddNumber {
-    public int evenAndOddNumber(int number){
+    public int evenAndOddNumber(int number) {
         return number;
     }
-    public static void main(String[] args) {
-        evenAndOddNumber evenAndOddNumber=new evenAndOddNumber();
-        int numberMatch=evenAndOddNumber.evenAndOddNumber(6);
 
-//        for (int i=0;i<=numberMatch;i++){
-//            if (i%2==0){
-//                System.out.println("Print Then Even Number :" +i);
-//            }
-//            else {
-//                System.out.println("Print the odd number :" +i);
-//            }
-          while (true){
-            if (numberMatch%2==0){
-                System.out.println("Print Then Even Number :" +numberMatch);
-                break;
-            }
-            else {
-                System.out.println("Print the odd number :" +numberMatch);
-                break;
+    public void printSingleEvenOrOdd() {
+        int numberMatch = evenAndOddNumber(6);
+
+        if (numberMatch % 2 == 0) {
+            System.out.println("Print the Even Number: " + numberMatch);
+        } else {
+            System.out.println("Print the Odd Number: " + numberMatch);
+        }
+    }
+
+    public void printEvenAndOdd() {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Even Number is : " + i);
+            } else {
+                System.out.println("Odd Number Is : " + i);
             }
         }
+    }
+    public void printNumberFromInputClass(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please input number : ");
+        int number=sc.nextInt();
+
+            if (number % 2 == 0) {
+                System.out.println("Even Number is : " + number);
+
+            } else {
+                System.out.println("Odd Number Is : " + number);
+
+            }
+            sc.close();
+
+    }
+
+    public static void main(String[] args) {
+        evenAndOddNumber evenAndOddNumber = new evenAndOddNumber();
+//        evenAndOddNumber.printSingleEvenOrOdd();
+//        evenAndOddNumber.printEvenAndOdd();
+        evenAndOddNumber.printNumberFromInputClass();
     }
 }
