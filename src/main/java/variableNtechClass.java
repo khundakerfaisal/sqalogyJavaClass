921c1b9378f1e80fe035143a7553bfb277513e17
khundakerfaisal <khundakermis@gmail.com>
2025-09-17 18:53:50 +0600

    added variable

---
+++ src/main/java/variableNtechClass.java
@@ -0,0 +1,29 @@
+public class variableNtechClass {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        car1.brand = "Toyota";
+        car1.modal = "Premio";
+
+        car2.brand = "Pazero";
+        car2.modal = "axios";
+
+        System.out.println("Brand: " + car1.brand);
+        System.out.println("Modal: " + car1.modal);
+        System.out.println("Brand: " + car2.brand);
+        System.out.println("Modal: " + car2.modal);
+
+        System.out.println(car1.modal + "has" + car1.wheel + "wheel");
+        System.out.println(car2.modal + "has" + car2.wheel + "wheel");
+
+        purchase(car1.brand,car2.modal)
+
+    }
+
+}
+
+class Car {
+    String brand;
+    String modal;
+    static int wheel = 4;
+}
