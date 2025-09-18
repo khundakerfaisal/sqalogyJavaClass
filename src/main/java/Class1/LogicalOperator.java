a56b1ec722cf2047dba0063c0ecddf7b81915335
khundakerfaisal <khundakermis@gmail.com>
2025-09-18 19:59:26 +0600

    added logical operator code

---
+++ src/main/java/Class1/LogicalOperator.java
@@ -0,0 +1,22 @@
+package Class1;
+
+import java.util.Scanner;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please input age: ");
+        int value = sc.nextInt();
+
+        boolean age = value <= 16;  // Check if age is 16 or less
+
+        if (age) {
+            System.out.println("You have input the wrong age");
+        } else {
+            System.out.println("You have input the correct age");
+        }
+
+        sc.close();
+    }
+}
