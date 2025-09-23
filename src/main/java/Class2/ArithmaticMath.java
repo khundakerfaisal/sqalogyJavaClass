3dc227a3be6517a8fe6bfab1d221f533ac6f266e
khundakerfaisal <khundakermis@gmail.com>
2025-09-23 19:35:23 +0600

    added mat function

---
+++ src/main/java/Class2/ArithmaticMath.java
@@ -0,0 +1,11 @@
+package Class2;
+
+public class ArithmaticMath {
+    public static void main(String[] args) {
+        double a=9.00;
+        double b=7.00;
+        System.out.println("Square Value is: "+Math.sqrt(a));
+        System.out.println("Maximum Value is: "+Math.max(a,b));
+
+    }
+}
