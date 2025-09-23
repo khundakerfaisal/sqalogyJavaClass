59e73f789763e4a4bdbaa8a26b3e14ca4fe5e017
khundakerfaisal <khundakermis@gmail.com>
2025-09-23 19:42:35 +0600

    added math function

--- src/main/java/Class2/ArithmaticMath.java
+++ src/main/java/Class2/ArithmaticMath.java
@@ -1,11 +1,21 @@
 package Class2;
 
+import java.util.Random;
+
 public class ArithmaticMath {
     public static void main(String[] args) {
         double a=9.00;
         double b=7.00;
         System.out.println("Square Value is: "+Math.sqrt(a));
         System.out.println("Maximum Value is: "+Math.max(a,b));
+        System.out.println("Minimum Value is: "+Math.min(a,b));
+
+
+        double generateNumber=Math.random()*100;
+        System.out.println("Random number is "+generateNumber);
+        System.out.println("Floor number is "+Math.floor(generateNumber));
+        System.out.println("Ceil number is "+Math.ceil(generateNumber));
+        System.out.println("Round number is "+Math.round(generateNumber));
 
     }
 }
