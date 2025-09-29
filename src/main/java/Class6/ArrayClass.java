94adedaab53fd2cc9ad31418fb3d8c3951ad8f41
khundakerfaisal <khundakermis@gmail.com>
2025-09-29 19:41:56 +0600

    added function and one dimension array

---
+++ src/main/java/Class6/ArrayClass.java
@@ -0,0 +1,13 @@
+package Class6;
+
+public class ArrayClass {
+    public static void main(String[] args) {
+        int [] number=new int[5];
+        number[0]=10;
+        number[1]=20;
+        number[2]=30;
+        for (int i=0;i<number.length;i++){
+            System.out.println(number[i]);
+        }
+    }
+}
---
+++ src/main/java/Class6/FunctionClas.java
@@ -0,0 +1,13 @@
+package Class6;
+
+public class FunctionClas {
+    public void sumAmount(int a,int b){
+        int result=a*b;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        FunctionClas functionClass=new FunctionClas();
+        functionClass.sumAmount(5,4);
+
+    }
+}
