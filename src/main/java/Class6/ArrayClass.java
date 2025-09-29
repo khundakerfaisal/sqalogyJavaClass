28abb19bc6ae05a0889f6639135f38f473bdf4e6
khundakerfaisal <khundakermis@gmail.com>
2025-09-29 19:51:24 +0600

    added array finding and print

--- src/main/java/Class6/ArrayClass.java
+++ src/main/java/Class6/ArrayClass.java
@@ -1,13 +1,19 @@
 package Class6;
 
+import java.util.Arrays;
+
 public class ArrayClass {
     public static void main(String[] args) {
-        int [] number=new int[5];
-        number[0]=10;
-        number[1]=20;
-        number[2]=30;
+        int [] number={10,20,30};
         for (int i=0;i<number.length;i++){
-            System.out.println(number[i]);
+            System.out.println(number[i]); //Print all number
         }
+        System.out.println(number[0]);
+//        number[0]=10;
+//        number[1]=20;
+//        number[2]=30;
+//        for (int i=0;i<number.length;i++){
+//            System.out.println(number[i]);
+//        }
     }
 }
