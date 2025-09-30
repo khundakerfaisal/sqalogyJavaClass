80a63e6e6b08860e12189dc9e931f4c45833204c
khundakerfaisal <khundakermis@gmail.com>
2025-09-30 20:32:19 +0600

    added hashmap

---
+++ src/main/java/Class7/ArrayHashMap.java
@@ -0,0 +1,14 @@
+package Class7;
+
+import java.util.HashMap;
+
+public class ArrayHashMap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> fruits=new HashMap<>();
+        fruits.put("Apple",350);
+        fruits.put("Apple",350);
+        fruits.put("Mango",120);
+        fruits.put("Orange",300);
+        System.out.println(fruits);
+    }
+}
---
+++ src/main/java/Class7/ArrayHashSet.java
@@ -0,0 +1,17 @@
+package Class7;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayHashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> set=new HashSet<>();
+        set.add(10);
+        set.add(10);
+        set.add(20);
+        System.out.println(set);
+
+
+
+    }
+}
