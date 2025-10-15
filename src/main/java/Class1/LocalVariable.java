b865e2b0374eb3fe42abfebd60bc554659327a91
khundakerfaisal <khundakermis@gmail.com>
2025-10-15 19:18:01 +0600

    added instance variable

--- src/main/java/Class1/LocalVariable.java
+++ src/main/java/Class1/LocalVariable.java
@@ -1,14 +1,27 @@
 package Class1;
 
 public class LocalVariable {
-    public void showAge() {
-        int age = 30; //local variable
-        System.out.println(age);
+    String name = "Omar sani";
+    public void display() {
+        System.out.println(name);
     }
-
     public static void main(String[] args) {
-        LocalVariable localVariable=new LocalVariable();
-        localVariable.showAge();
+        LocalVariable localVariable = new LocalVariable();
+        localVariable.display();
 
     }
 }
+
+//    public void showAge() {
+//        int age = 30; //local variable
+//        System.out.println(age);
+//    }
+//
+//    public static void main(String[] args) {
+//        LocalVariable localVariable=new LocalVariable();
+//        localVariable.showAge();
+//
+//
+//
+//    }
+//}
