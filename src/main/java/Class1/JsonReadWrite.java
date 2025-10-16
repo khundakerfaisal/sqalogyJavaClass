7eff653635e99a23fd4b0e1905df2eba3fd41816
khundakerfaisal <khundakermis@gmail.com>
2025-10-16 20:30:09 +0600

    added read file

--- src/main/java/Class1/JsonReadWrite.java
+++ src/main/java/Class1/JsonReadWrite.java
@@ -13,12 +13,28 @@ import java.io.IOException;
 public class JsonReadWrite {
     public static void main(String[] args) throws IOException, ParseException {
         String fileName="src/test/resources/createUser.json";
-        String userName="UserName-7";
+        String prefixName="UserName-";
+        int countNumber=0;
+        int generateNumber=2;
+        String generateUserName="";
         JSONParser parser=new JSONParser();
         JSONArray lastNameArray= (JSONArray) parser.parse(new FileReader(fileName));
+        if (!lastNameArray.isEmpty()){
+            JSONObject lastNameObject= (JSONObject) lastNameArray.get(lastNameArray.size()-1);
+            String lastUserName=lastNameObject.get("lastUserName").toString();
+            countNumber= Integer.parseInt(lastUserName.replace(prefixName,""));
+
+        }
+        else {
+            System.out.println("User Created  successfully");
+        }
+
+        for (int i=0;i<=generateNumber;i++){
+          generateUserName=prefixName+(countNumber+i) ;
+        }
         JSONObject lastObject=new JSONObject();
 
-        lastObject.put("lastUserName",userName);
+        lastObject.put("lastUserName",generateUserName);
         lastNameArray.add(lastObject);
         FileWriter writer=new FileWriter(fileName);
         writer.write(lastNameArray.toJSONString());
