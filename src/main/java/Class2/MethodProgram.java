c001ac4595949da2f355e569de6a096d90996aee
khundakerfaisal <khundakermis@gmail.com>
2025-10-19 19:31:14 +0600

    added method

---
+++ src/main/java/Class2/MethodProgram.java
@@ -0,0 +1,12 @@
+package Class2;
+
+public class MethodProgram {
+    public static int addNumber(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        int sum=addNumber(5,10);
+        System.out.println(sum);
+    }
+}
