ce500bc934cba12eef2994857d596a1d125219d0
khundakerfaisal <khundakermis@gmail.com>
2025-10-21 19:01:59 +0600

    added input user

---
+++ src/main/java/Class1/UserInput.java
@@ -0,0 +1,15 @@
+package Class1;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter First name : ");
+        String firstName = input.nextLine();
+        System.out.println("Enter Last name : ");
+        String lastName = input.nextLine();
+
+        System.out.println("Full Name is : " + firstName + " " + lastName);
+    }
+}
