9cf61aa03a1c5cf924ee5406ec15e39af336f922
khundakerfaisal <khundakermis@gmail.com>
2025-10-22 19:16:13 +0600

    added second largest number

---
+++ src/main/java/Class2/SecondLargestNumber.java
@@ -0,0 +1,19 @@
+package Class2;
+
+public class SecondLargestNumber {
+    public static void main(String[] args) {
+        int[] numbers = {6, 3, 9, 14, 15, 1, 8, 5};
+        int max = 0;
+        int sLargest = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                sLargest = max;
+                max = numbers[i];
+            }
+
+            }
+        System.out.println(sLargest);
+
+
+    }
+}
