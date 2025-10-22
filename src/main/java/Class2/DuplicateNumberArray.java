693d81b74e96c7a729fda0cd7f5dc06e11ebcf3b
khundakerfaisal <khundakermis@gmail.com>
2025-10-22 19:17:53 +0600

    added duplicate array code

---
+++ src/main/java/Class2/DuplicateNumberArray.java
@@ -0,0 +1,15 @@
+package Class2;
+
+public class DuplicateNumberArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 4, 5, 1, 2};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    System.out.println(numbers[i]);
+                }
+            }
+        }
+    }
+
+}
