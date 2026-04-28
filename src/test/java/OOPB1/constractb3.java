a2000c74d7ef2e5480b91d667f1db3e42cbc7d5e
khundakerfaisal <khundakermis@gmail.com>
2026-04-28 17:45:29 +0600

    added constract program

---
+++ src/test/java/OOPB1/constractb3.java
@@ -0,0 +1,11 @@
+package OOPB1;
+
+public class constractb3 {
+    public constractb3(){
+        System.out.println("first call the consctructor");
+    }
+    public static void main(String[] args) {
+        new constractb3();
+        System.out.println("second call main method");
+    }
+}
